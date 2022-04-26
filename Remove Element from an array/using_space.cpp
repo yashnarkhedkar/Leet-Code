713c0e878158e26e770c75feae6c3699344334d7
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-04-27 01:36:39 +0530

    Create using_space.cpp

---
+++ Remove Element from an array/using_space.cpp
@@ -0,0 +1,14 @@
+int removeElement(vector<int>&nums, int val)
+{
+  vector<int>v;
+  
+  for(int i = 0; i < nums.size(); i++)
+  {
+    if(nums[i] == val)
+    {
+      v.push_back(nums[i]);
+    }
+  }
+  nums = v;
+  return nums.size();
+}
