14fc0f13a51672cb01313cf13928ac9169082db8
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-04-28 00:01:30 +0530

    Create code.cpp

---
+++ Plus One/code.cpp
@@ -0,0 +1,20 @@
+vector<int> plusOne(vector<int>digits)
+{
+  int n = digits.size() - 1;
+  
+  for(int i = n; i >= 0; i--)
+  {
+      if(digits[i] != 9)
+      {
+          digits[i]++;
+          return digits;
+      }
+      else
+      {
+        digits[i] = 0;
+      }
+  }
+  
+  digits.insert(digits.begin(), 1);
+  return digits;
+}
