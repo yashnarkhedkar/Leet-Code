04b904cc9f98ccab43722ed3031bdf8447e08984
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-04-29 19:44:32 +0530

    Create code.cpp

---
+++ Find Smallest Letter Greater Than Target/code.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int s = 0;
+        int e = letters.size() - 1;
+        char ans;
+        int mid;
+        
+        if(target - 'a' >= letters[e] - 'a')
+        {
+            return letters[0];
+        }
+        
+        while(s <= e)
+        {
+            mid = s + (e-s)/2;
+            
+            if(letters[mid] - 'a' > target - 'a' )
+            {
+                ans = letters[mid];
+                e = mid - 1;
+            }
+            
+            else
+            {
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
