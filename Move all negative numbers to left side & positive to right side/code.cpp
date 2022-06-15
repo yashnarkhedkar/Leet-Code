c8c5c83215f074a4abd10909d656ce66da5fcf12
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-06-15 17:41:41 +0530

    Create code.cpp

---
+++ Move all negative numbers to left side & positive to right side/code.cpp
@@ -0,0 +1,14 @@
+vector<int> separateNegativeAndPositive(vector<int> &nums){
+    // Write your code here.
+    int j = 0;
+    
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] < 0){
+            if(i != j){
+                swap(nums[i], nums[j]);
+            }
+            j++;
+        }
+    }
+    return nums;
+}
