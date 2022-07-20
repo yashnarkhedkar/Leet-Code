02e05a51ce15b4de6e7e41ba3dbb408c7006cd84
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2022-07-20 22:24:54 +0530

    Create code.cpp

---
+++ stack permutation/code.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        int j = 0;
+        stack<int>s;
+        
+        for(int i = 0; i < N; i++){
+            s.push(A[i]);
+            while(!s.empty() && s.top() == B[j]){
+               s.pop(); 
+               j++;
+            }
+        }
+        return s.empty();
+    }
+};
